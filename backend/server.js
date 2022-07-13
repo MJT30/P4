38d611ca1902b98b24b112da5ffcef300b7162f8
mylest913 <mylest913@gmail.com>
2022-07-13 08:15:42 -0700

    Changing frontend

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,30 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const aniFoodSeed = require("./db/seed.json");
+require("hbs");
 const AniFood = require("./models/seed");
+const methodOverride = require("method-override");
 const app = express();
+app.use(express.static("backend" + "public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.set("view engine", "hbs");
 
-app.get("/api", (req, res) => {
-  //<------ Shows the api data as JSON, won't be accessible my user ------>//
-  AniFood.find({}).then((data) => res.send(aniFoodSeed));
+app.use(methodOverride("_method"));
+
+//Landing page
+app.get("/home", (req, res) => {
+  AniFood.find({}).then((data) => res.render("anifood/landing", { data }));
 });
 
-app.post("/new", (req, res) => {
-  AniFood.create(req.body);
-  res.redirect("/api");
-});
+// app.get("/api", (req, res) => {
+//   //<------ Shows the api data as JSON, won't be accessible my user ------>//
+//   AniFood.find({}).then((data) => res.send(aniFoodSeed));
+// });
+
+// app.post("/new", (req, res) => {
+//   AniFood.create(req.body);
+//   res.redirect("/api");
+// });
 
 app.listen(4000, () => console.log("Server is up and running"));
