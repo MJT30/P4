3090d2c00a7d2b9e1497d1da6a55328373802af9
mylest913 <mylest913@gmail.com>
2022-07-13 18:27:41 -0700

    Changed from react to HBS got the front end torender. Will now add functionality for comments and search bar

--- server.js
+++ server.js
@@ -5,16 +5,41 @@ require("hbs");
 const AniFood = require("./models/seed");
 const methodOverride = require("method-override");
 const app = express();
-app.use(express.static("backend" + "public"));
+app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "hbs");
 
 app.use(methodOverride("_method"));
 
-//Landing page
+//Landing page.
 app.get("/home", (req, res) => {
   AniFood.find({}).then((data) => res.render("anifood/landing", { data }));
+});
+
+//Takes you to page where you can add new recipe.
+app.get("/newrecipe", (req, res) => {
+  res.render("anifood/newrecipe");
+  console.log("Adding a new post");
+});
+
+//Finds the id of content. Also sends you to the "viewrecipe" route.
+app.get("/home/:id/", (req, res) => {
+  AniFood.findById(req.params.id)
+    .then((data) => {
+      console.log(data);
+      res.render("anifood/viewrecipe", { data });
+    })
+    .catch(console.error);
+  console.log("I'm working");
+});
+
+//Post's new recipe to homepage.
+app.post("/home", (req, res) => {
+  console.log(req.body);
+  AniFood.create(req.body);
+  res.redirect("/home");
+  console.log("Adding new recipe");
 });
 
 // app.get("/api", (req, res) => {
