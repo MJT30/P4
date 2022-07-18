78870d3777d84f66803a35f97da41f119bf9ef74
mylest913 <mylest913@gmail.com>
2022-07-17 23:11:52 -0700

    redoing frontend...again

--- frontend/ani-food/src/components/Content.jsx
+++ frontend/ani-food/src/components/Content.jsx
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from "react";
 import "./header.css";
 const Content = () => {
-  const [backEnd, setBackend] = useState([{}]);
+  const [backEnd, setBackend] = useState(null);
   useEffect(() => {
-    fetch("/api") //<------ Fetching data from my backend that is listining on port 4000 & the proxy is defined in the package.json file ------>//
+    fetch("/home") //<------ Fetching data from my backend that is listining on port 4000 & the proxy is defined in the package.json file ------>//
       .then((response) => response.json())
       .then((data) => {
         console.log(data);
@@ -11,20 +11,21 @@ const Content = () => {
       });
   }, []);
 
-  return <div><>
-  <div className="content">
-
-  <h1>{backEnd[0].dish}</h1>
-  <h5>{backEnd[0].anime}</h5>
-  <img className="image" src={backEnd[0].anime_image} alt="..."/>
-  <img className="image" src={backEnd[0].rl_image} alt="..."/>
-  <p className="recipe">
-    {backEnd[0].recipe}
-  </p>
-  </div>
-    
-  </></div>;
-  
+  return (
+    <div>
+      {backEnd && (
+        <>
+          <div className="content">
+            <h1>{backEnd[2].dish}</h1>
+            <h5>{backEnd[2].anime}</h5>
+            <img className="image" src={backEnd[2].anime_image} alt="..." />
+            <img className="image" src={backEnd[2].rl_image} alt="..." />
+            <p className="recipe">{backEnd[2].recipe}</p>
+          </div>
+        </>
+      )}
+    </div>
+  );
 };
 
 export default Content;
