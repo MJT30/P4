5e30243d4d608351fd8515ac0934fffdbe75af7a
mylest913 <mylest913@gmail.com>
2022-07-17 22:02:46 -0700

    commit

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const mongoose = require("mongoose");
 const fileUpload = require("express-fileupload");
 const path = require("path");
 
-const mid = require("./middleware/mid");
-const sizeLimit = require("./middleware/sizeLimit");
-const extentionLimit = require("./middleware/extentionLimit");
+// const mid = require("./middleware/mid");
+// const sizeLimit = require("./middleware/sizeLimit");
+// const extentionLimit = require("./middleware/extentionLimit");
 
 const aniFoodSeed = require("./db/seed.json");
 require("hbs");
@@ -25,6 +25,16 @@ app.get("/home", (req, res) => {
   AniFood.find({}).then((data) => res.render("anifood/landing", { data }));
 });
 
+// app.post("/home", async (req, res) => {
+//   let payload = req.body.payload.trim();
+//   let query = await AniFood.find({
+//     dish: { $regex: new RegExp("^" + payload + ".*", "i") },
+//   }).exec();
+//   query = query.slice(0, 10);
+//   res.send({ payload: search });
+//   console.log(payload);
+// });
+
 //Takes you to page where you can add new recipe.
 app.get("/newrecipe", (req, res) => {
   res.render("anifood/newrecipe");
@@ -43,38 +53,38 @@ app.get("/home/:id/", (req, res) => {
 });
 
 //Post's new recipe to homepage.
-// app.post("/home", (req, res) => {
-//   console.log(req.body);
-//   AniFood.create(req.body);
-//   res.redirect("/home");
-//   console.log("Adding new recipe");
-// });
-
-//Upload route
 app.post("/home", (req, res) => {
   console.log(req.body);
   AniFood.create(req.body);
   res.redirect("/home");
   console.log("Adding new recipe");
 });
-fileUpload({ createParentPath: true }),
-  mid,
-  extentionLimit([".png", ".jpg", ".jpeg"]),
-  sizeLimit,
-  (req, res) => {
-    const files = req.files;
-    console.log(files);
-    Object.keys(files).forEach((key) => {
-      const filePath = path.join(__dirname, "files", files[key].name);
-      files[key].mv(filePath),
-        (err) => {
-          if (err)
-            return res.status(500).json({ status: "error", message: err });
-        };
-    });
 
-    return res.json({ status: "in there", message: "also in there" });
-  };
+//Upload route
+// app.post("/home", (req, res) => {
+//   console.log(req.body);
+//   AniFood.create(req.body);
+//   res.redirect("/home");
+//   console.log("Adding new recipe");
+// });
+// fileUpload({ createParentPath: true }),
+//   mid,
+//   extentionLimit([".png", ".jpg", ".jpeg"]),
+//   sizeLimit,
+//   (req, res) => {
+//     const files = req.files;
+//     console.log(files);
+//     Object.keys(files).forEach((key) => {
+//       const filePath = path.join(__dirname, "files", files[key].name);
+//       files[key].mv(filePath),
+//         (err) => {
+//           if (err)
+//             return res.status(500).json({ status: "error", message: err });
+//         };
+//     });
+
+//     return res.json({ status: "in there", message: "also in there" });
+//   };
 
 // app.get("/api", (req, res) => {
 //   //<------ Shows the api data as JSON, won't be accessible my user ------>//
